b30ab482fafe503f55b6d8eee9016ba57fe4c7dc
Kaman Khadka <kaman.khadka@crystalsolution.com.np>
2022-02-15 10:37:42 +0545

    Add project files.

---
+++ User Management/Areas/User/Controllers/RegisterController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace User_Management.Areas.User.Controllers
+{
+    [Area("User")]
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ User Management/Areas/User/Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User_Management.Areas.User.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and Confirm Password didnot match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
